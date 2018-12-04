6d49030aa6be67c3118e58eb237213ce5cca2254
jun-1 <jun-1@DESKTOP-21UO3OU>
2018-12-04 09:30:51 +0800

    TravisTesting

---
+++ travisTesting/src/travistesting/TravisTesting.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travistesting;
+
+/**
+ *
+ * @author jun-1
+ */
+public class TravisTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello World");
+    }
+    
+}
